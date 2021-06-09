8f167877e5795a2d7bb50930c3d3a7aecf80a3f2
Karlatemp <karlatemp@vip.qq.com>
2021-06-09 14:08:50 +0800

    CLI point

---
+++ src/main/java/io/github/karlatemp/kasukufzos/cli/Launch.java
@@ -0,0 +1,27 @@
+package io.github.karlatemp.kasukufzos.cli;
+
+import io.github.karlatemp.kasukufzos.image.reader.KFzClassLoader;
+import io.github.karlatemp.kasukufzos.image.reader.KFzReader;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class Launch {
+    public static void main(String[] args) throws Throwable {
+        KFzClassLoader.Options options = new KFzClassLoader.Options();
+        options.signAction = KFzClassLoader.Options.SignAction.THROW_ON_FAILURE;
+        KFzClassLoader classLoader = new KFzClassLoader(Launch.class.getClassLoader(),
+                KFzReader.from(new File(args[0])), options
+        );
+        Class<?> aClass = classLoader.loadClass(args[1]);
+        Method main = aClass.getMethod("main", String[].class);
+        String[] copy = Arrays.copyOfRange(args, 2, args.length);
+        try {
+            main.invoke(null, (Object) copy);
+        } catch (InvocationTargetException e) {
+            throw e.getTargetException();
+        }
+    }
+}
---
+++ src/main/java/io/github/karlatemp/kasukufzos/cli/Zip.java
@@ -0,0 +1,38 @@
+package io.github.karlatemp.kasukufzos.cli;
+
+import io.github.karlatemp.kasukufzos.image.writer.KFzWriter;
+import io.github.karlatemp.kasukufzos.utils.TransferKit;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.zip.ZipFile;
+
+public class Zip {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.out.println("Usage:");
+            System.out.println();
+            System.out.println("java ..... [output location] [library] [library] ....");
+        }
+        File output = new File(args[0]);
+        KFzWriter writer = KFzWriter.of(output);
+        for (String path : Arrays.asList(args).subList(1, args.length)) {
+            emit(new File(path), writer);
+        }
+        writer.close();
+    }
+
+    private static void emit(File file, KFzWriter writer) throws Exception {
+        if (file.isFile()) {
+            System.out.println("Emitting " + file);
+            try (ZipFile zf = new ZipFile(file)) {
+                TransferKit.transfer(zf, file.getName(), writer);
+            }
+            System.out.println("Emitted  " + file);
+        } else if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files == null) return;
+            for (File f : files) emit(f, writer);
+        }
+    }
+}
